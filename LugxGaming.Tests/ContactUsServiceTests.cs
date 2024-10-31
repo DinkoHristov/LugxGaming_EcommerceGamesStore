7064357a7fa42345cf931b6f5d641c015134844f
DinkoHristov <114079361+DinkoHristov@users.noreply.github.com>
2024-10-31 16:35:28 +0200

    Add test for the ContactUsService. 3 out of 8 Serices are tested now.

---
+++ LugxGaming.Tests/ContactUsServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using LugxGaming.Models;
+using LugxGaming.Services;
+
+namespace LugxGaming.Tests
+{
+    public class ContactUsServiceTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public async Task Test_SendEmailWithQuestionAsync()
+        {
+            var messageModel = new MessageFormModel()
+            {
+                Name = "Ivan",
+                Surname = "Ivanov",
+                Email = "Ivanov@gmail.com",
+                Subject = "Test message",
+                Message = "Test message description"
+            };
+
+            var contactUsService = new ContactUsService();
+            var result = await contactUsService.SendEmailWithQuestionAsync(messageModel);
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}
